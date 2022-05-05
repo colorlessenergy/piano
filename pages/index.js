833ea38e049a3ca6fdfdac5f7d537487da75f50a
colorlessenergy <brianmunozdev@gmail.com>
2022-05-05 13:57:34 -0700

    add HTML and CSS for piano

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,26 @@ export default function Home() {
                 <meta name="description" content="piano" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+
+            <div className="container">
+                <div className="piano">
+                    <div className="piano-top"></div>
+                    <div className="piano-bottom">
+                        <div className="piano-white-key"></div>
+                        <div className="piano-black-key piano-black-key-0"></div>
+                        <div className="piano-white-key"></div>
+                        <div className="piano-black-key piano-black-key-1"></div>
+                        <div className="piano-white-key"></div>
+                        <div className="piano-white-key"></div>
+                        <div className="piano-black-key piano-black-key-2"></div>
+                        <div className="piano-white-key"></div>
+                        <div className="piano-black-key piano-black-key-3"></div>
+                        <div className="piano-white-key"></div>
+                        <div className="piano-black-key piano-black-key-4"></div>
+                        <div className="piano-white-key"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
